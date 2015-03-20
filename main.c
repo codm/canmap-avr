9377c090d7fd51d29424bf17b54c6ee88489bb2b
Tobias Schmitt <ts@codm.de>
2015-03-20 10:30:13 +0100

    canblocks send and definitions

--- main.c
+++ main.c
@@ -3,123 +3,87 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 
+#include "main.h"
 #include "can.h"
 
-// -----------------------------------------------------------------------------
-/** Set filters and masks.
- *
- * The filters are divided in two groups:
- *
- * Group 0: Filter 0 and 1 with corresponding mask 0.
- * Group 1: Filter 2, 3, 4 and 5 with corresponding mask 1.
- *
- * If a group mask is set to 0, the group will receive all messages.
- *
- * If you want to receive ONLY 11 bit identifiers, set your filters
- * and masks as follows:
- *
- *	prog_uint8_t can_filter[] = {
- *		// Group 0
- *		MCP2515_FILTER(0),				// Filter 0
- *		MCP2515_FILTER(0),				// Filter 1
- *		
- *		// Group 1
- *		MCP2515_FILTER(0),				// Filter 2
- *		MCP2515_FILTER(0),				// Filter 3
- *		MCP2515_FILTER(0),				// Filter 4
- *		MCP2515_FILTER(0),				// Filter 5
- *		
- *		MCP2515_FILTER(0),				// Mask 0 (for group 0)
- *		MCP2515_FILTER(0),				// Mask 1 (for group 1)
- *	};
- *
- *
- * If you want to receive ONLY 29 bit identifiers, set your filters
- * and masks as follows:
- *
- * \code
- *	prog_uint8_t can_filter[] = {
- *		// Group 0
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 0
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 1
- *		
- *		// Group 1
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 2
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 3
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 4
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 5
- *		
- *		MCP2515_FILTER_EXTENDED(0),		// Mask 0 (for group 0)
- *		MCP2515_FILTER_EXTENDED(0),		// Mask 1 (for group 1)
- *	};
- * \endcode
- *
- * If you want to receive both 11 and 29 bit identifiers, set your filters
- * and masks as follows:
- */
-const prog_uint8_t can_filter[] = 
-{
-	// Group 0
-	MCP2515_FILTER(0),				// Filter 0
-	MCP2515_FILTER(0),				// Filter 1
-	
-	// Group 1
-	MCP2515_FILTER_EXTENDED(0),		// Filter 2
-	MCP2515_FILTER_EXTENDED(0),		// Filter 3
-	MCP2515_FILTER_EXTENDED(0),		// Filter 4
-	MCP2515_FILTER_EXTENDED(0),		// Filter 5
-	
-	MCP2515_FILTER(0),				// Mask 0 (for group 0)
-	MCP2515_FILTER_EXTENDED(0),		// Mask 1 (for group 1)
-};
-// You can receive 11 bit identifiers with either group 0 or 1.
+uint8_t self = 0x01;
+uint8_t rec = 0xff;
 
+CANBLOCKS_MESSAGE cbm;
+
+int canblocks_receive(CANBLOCKS_MESSAGE *msg)
+{
+  int len = 0;
+  can_t getm;
+  can_get_message(&getm);
+  if(getm.data[1] == CANP_SYNC) {
+    if(getm.data[2] == CANBLOCKS_START) {
+      canblocks_emtpy_data(msg);
+    }
+  }
+}
+
+int canblocks_send(CANBLOCKS_MESSAGE msg) {
+  can_t sendmsg;
+  msg.send = self;
+  msg.rec = rec;
+  msg.data = "herzliches Hallo vom Sender an den Empfaenger";
+  msg.command = CANP_SYNC;
+  msg.blocklen = sizeof(msg.data)/sizeof(uint8_t);
+  msg.timer = 0;
+  msg.status = CANBLOCKSM_STATE_READY;
+
+  sendmsg.id = msg.rec;
+  sendmsg.data[0] = msg.send;
+  sendmsg.data[1] = msg.command;
+  if(msg.command == CANP_SYNC) 
+  {
+    while(*dataptr != '\0') 
+    {
+      int iter_send;
+      for(iter_send = 2; iter_send < 8; iter_send++) 
+      {
+        sendmsg.data[iter_send] = dataptr++;
+      }
+    }
+  }
+  else
+  {
+    int iter_send;
+    for(iter_send = 2; iter_send < 8; iter_send++)
+    {
+      sendmsg.data[iter_send] = msg.data[iter_send - 2];
+    }
+  }
+  can_send_message(&sendmsg);
+}
+
+}
+
+void canblocks_emtpy_data(CANBLOCKS_MESSAGE *msg) {
+  msg->status = ready;
+  msg->timer = 0;
+  msg->blocklen = 0;
+  msg->command = 0;
+  msg->send = 0;
+  msg->rec = 0;
+  for(i = 0; i++; i < CANBLOCKS_MAX) {
+    msg->data[i] = 0;
+  }
+}
 
 // -----------------------------------------------------------------------------
 // Main loop for receiving and sending messages.
 
 int main(void)
 {
-	// Initialize MCP2515
-	can_init(BITRATE_125_KBPS);
-	
-	// Load filters and masks
-	//can_static_filter(can_filter);
-	
-	// Create a test messsage
-	can_t msg;
-	
-	msg.id = 0x123456;
-	msg.flags.rtr = 0;
-	msg.flags.extended = 1;
-	
-	msg.length = 4;
-	msg.data[0] = 0xde;
-	msg.data[1] = 0xad;
-	msg.data[2] = 0xbe;
-	msg.data[3] = 0xef;
-	
-	// Send the message
-	can_send_message(&msg);
-	
-	while (1)
-	{
-		// Check if a new messag was received
-		if (can_check_message())
-		{
-			can_t msg;
-			
-			// Try to read the message
-			if (can_get_message(&msg))
-			{
-				// If we received a message resend it with a different id
-				msg.id += 10;
-				
-				// Send the new message
-				can_send_message(&msg);
-			}
-		}
-	}
-	
-	return 0;
+  // Initialize MCP2515
+  can_init(BITRATE_125_KBPS);
+  can_send_message(&msg);
+  while (1)
+  {
+    canblocks_receive(cbm);
+  }
+  return 0;
 }
+
---
+++ main.h
@@ -0,0 +1,48 @@
+/*
+  CAN Definitions
+  */
+
+#define CANP_REQUEST(a) ( a < 0x42 );
+#define CANP_REQANSWER 0x43
+#define CANP_BLINK 0x44
+#define CANP_RESET 0x45
+#define CANP_SYNC 0xFA
+#define CANP_ACK 0xFB
+
+#define CANBLOCKS_MAX 255
+#define CANBLOCKS_START 0xFF
+#define CANBLOCKS_END 0xFE
+
+#define CANBLOCKST_STRING 0x01
+
+#define CANBLOCKSM_STATE_READY 0x00
+#define CANBLOCKSM_STATE_TRANS 0x01
+#define CANBLOCKSM_STATE_FIN   0x02
+
+#define CAN_SELF 0x01
+#define CAN_OTHER 0x02
+
+
+
+
+/*
+  Typedefinitions
+  */
+
+typedef struct {
+    uint8_t rec;
+    uint8_t send;
+    uint8_t command;
+    uint8_t data[6];
+} CANMESSAGE;
+
+typedef struct {
+    uint8_t rec;
+    uint8_t send;
+    uint8_t command;
+    uint8_t blocklen;
+    uint32_t timer;
+    char* data;
+    uint8_t status; // 0: waiting, 1: transmittions, 2: ready
+} CANBLOCKS_MESSAGE;
+
