6e3d83d4c9a834420601db5a4e65078c358c7574
Tobias Schmitt <tobias.schmitt@codm.de>
2015-04-09 15:31:10 +0200

    added header files and began can_init function

--- can.c
+++ can.c
@@ -1,3 +1,12 @@
-bool can_init(can_bitrate_t bitrate) {
-  return false;
+int can_init(can_bitrate_t bitrate) {
+  // set rx and tx pins
+  CAN_DDR &= ~(1 << CAN_RX);
+  CAN_DDR |= (1 << CAN_TX);
+
+  //Reset CAN Controller
+  CANGCON |= (1 << SWRES);
+  CANGCON = 0x00;
+
+
+  return 0;
 }
